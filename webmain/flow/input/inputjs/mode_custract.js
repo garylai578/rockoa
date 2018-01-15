f9e9a42cb4840d160fedc70df4cbe226dd6c5f3f
laiguanhui <garylai414@126.com>
2018-01-15 17:26:33 +0800

    我的合同里自动计算小计金额

--- webmain/flow/input/inputjs/mode_custract.js
+++ webmain/flow/input/inputjs/mode_custract.js
@@ -11,7 +11,30 @@ function initbodys(){
 	$(form('saleid')).change(function(){
 		salechange(this.value);
 	});
+
+/*    $(form('money')).click(function(){
+        countMoney();
+    });*/
+    $("[name^='money0_']").click(function(){
+    	countMoney();
+	})
 }
+
+/*
+ 计算合计金额，并进行动态更新。
+ */
+function countMoney(){
+    var d=c.getsubdata(0);
+    var to=0,i,len=d.length;
+    for(i=0;i<len;i++){
+        if(d[i].name!=''){
+            d[i].money = d[i].price * d[i].num;
+            form('money0_'+i).value=d[i].money+'';
+            to=to+parseFloat(d[i].money);
+        }
+    }
+}
+
 function salechange(v){
 	if(v==''){
 		form('custid').value='';

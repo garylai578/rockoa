a982c83fdccef32925c5bc504ebc9cdc0fe992a7
laiguanhui <garylai414@126.com>
2018-06-05 09:41:01 +0800

    增加导出Excel的设置：如果设置了id为excelContant的表格，则到处该表格的内容，否则到处系统默认的表格内容。

--- webmain/task/mode/modeview.js
+++ webmain/task/mode/modeview.js
@@ -198,8 +198,13 @@ var c={
 		js.location(url);
 	},
 	daochuexcel:function () {
-        var url='task.php?a=p&num='+modenum+'&mid='+mid+'&stype=excel';
-        js.location(url);
+        var curTbl = document.getElementById('excelContant');
+        if(curTbl) // 如果设置了需要到处的excel的内容，则到处指定的内容
+			exportExcel('excelContant');
+        else {		// 否则到处系统默认的表格
+			var url='task.php?a=p&num='+modenum+'&mid='+mid+'&stype=excel';
+			js.location(url);
+        }
     },
 	hideoth:function(){
 		$('.menulls').hide();
@@ -439,3 +444,77 @@ var c={
 		});
 	}
 };
+
+var idTmr;
+function  getExplorer(){
+    var explorer = window.navigator.userAgent ;
+    //ie
+    if (explorer.indexOf("MSIE") >= 0) {
+        return 'ie';
+    }
+    //firefox
+    else if (explorer.indexOf("Firefox") >= 0) {
+        return 'Firefox';
+    }
+    //Chrome
+    else if(explorer.indexOf("Chrome") >= 0){
+        return 'Chrome';
+    }
+    //Opera
+    else if(explorer.indexOf("Opera") >= 0){
+        return 'Opera';
+    }
+    //Safari
+    else if(explorer.indexOf("Safari") >= 0){
+        return 'Safari';
+    }
+}
+
+function exportExcel(tableid) {
+    if(getExplorer()=='ie'){
+        var curTbl = document.getElementById(tableid);
+        var oXL = new ActiveXObject("Excel.Application");
+        var oWB = oXL.Workbooks.Add();
+        var xlsheet = oWB.Worksheets(1);
+        var sel = document.body.createTextRange();
+        sel.moveToElementText(curTbl);
+        sel.select();
+        sel.execCommand("Copy");
+        xlsheet.Paste();
+        oXL.Visible = true;
+
+        try {
+            var fname = oXL.Application.GetSaveAsFilename("Excel.xls", "Excel Spreadsheets (*.xls), *.xls");
+        } catch (e) {
+            print("Nested catch caught " + e);
+        } finally {
+            oWB.SaveAs(fname);
+            oWB.Close(savechanges = false);
+            oXL.Quit();
+            oXL = null;
+            idTmr = window.setInterval("Cleanup();", 1);
+        }
+    }
+    else{
+        tableToExcel(tableid)
+    }
+}
+
+function Cleanup() {
+    window.clearInterval(idTmr);
+    CollectGarbage();
+}
+
+var tableToExcel = (function() {
+    var uri = 'data:application/vnd.ms-excel;base64,',
+        template = '<html><head><meta charset="UTF-8"></head><body><table>{table}</table></body></html>',
+        base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) },
+        format = function(s, c) {
+            return s.replace(/{(\w+)}/g,
+                function(m, p) { return c[p]; }) }
+    return function(table, name) {
+        if (!table.nodeType) table = document.getElementById(table)
+        var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+        window.location.href = uri + base64(format(template, ctx))
+    }
+})();
